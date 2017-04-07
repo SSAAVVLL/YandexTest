5edfdcf3dda66bbaaf36bbcf85f6d11f29466387
SSAAVVLL <SSAAVVLL@bk.ru>
2017-04-07 09:22:23 +0500

    Started writing translate request

--- app/src/main/java/com/ssaavvll/yandextest/TranslateFragment.java
+++ app/src/main/java/com/ssaavvll/yandextest/TranslateFragment.java
@@ -5,13 +5,16 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -20,6 +23,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,6 +31,9 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class TranslateFragment extends Fragment {
     public static final String APP_PREFERENCES = "mysettings";
@@ -35,9 +42,12 @@ public class TranslateFragment extends Fragment {
     protected SharedPreferences sPref;
     protected Spinner spinnerFrom;
     protected Spinner spinnerTo;
+    protected EditText textField;
     protected ImageButton buttonClear;
+    protected ConstraintLayout constraintLayout;
     protected HashMap<CharSequence, CharSequence> hmLang;
     protected ArrayAdapter<CharSequence> adapter;
+    protected Uri.Builder yandexTranslateRequestBuilder;
 
     private OnFragmentInteractionListener listener;
 
@@ -65,6 +75,8 @@ public class TranslateFragment extends Fragment {
         spinnerFrom = (Spinner) view.findViewById(R.id.spinnerFrom);
         spinnerTo = (Spinner) view.findViewById(R.id.spinnerTo);
         buttonClear = (ImageButton) view.findViewById(R.id.clearButton);
+        textField = (EditText) view.findViewById(R.id.editTextTrans);
+        constraintLayout = (ConstraintLayout) view.findViewById(R.id.translateResult);
         return view;
     }
 
@@ -74,7 +86,7 @@ public class TranslateFragment extends Fragment {
         adapter = new ArrayAdapter<CharSequence>(getActivity(), android.R.layout.simple_spinner_dropdown_item);
         hmLang = new HashMap<CharSequence, CharSequence>();
         /* Build uri for get list of langs from api */
-        Uri.Builder yandexTranslateBuilder = new Uri.Builder();
+        final Uri.Builder yandexTranslateBuilder = new Uri.Builder();
         yandexTranslateBuilder.scheme("https")
                 .authority("translate.yandex.net")
                 .appendPath("api")
@@ -87,6 +99,7 @@ public class TranslateFragment extends Fragment {
         /*Log.d("Test", yandexTranslateURL);*/
 
         /* get Json from http request, parse them */
+        //RequestFuture<JSONObject> future = RequestFuture.newFuture();
         JsonObjectRequest jsObjReq = new JsonObjectRequest
                 (Request.Method.GET, yandexTranslateURL, null, new Response.Listener<JSONObject>() {
                     @Override
@@ -106,6 +119,8 @@ public class TranslateFragment extends Fragment {
                                 /*Log.d("shortLang", hmLang.get(langName).toString());*/
                                 /*Log.d("lang", resp.get(langStr).toString());*/
                             }
+                            spinnerFrom.setSelection(sPref.getInt(APP_PREFERENCES_LANG_FROM, 0));
+                            spinnerTo.setSelection(sPref.getInt(APP_PREFERENCES_LANG_TO, 0));
                             /*Log.d("names", respNames.toString());
                             Log.d("resp", resp.get("ru").toString());
                             Log.d("respAll", resp.toString());
@@ -121,12 +136,49 @@ public class TranslateFragment extends Fragment {
                     }
                 });
         MySingleton.getInstance(getActivity()).addToRequestQueue(jsObjReq);
+        textField.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                Log.d("editText", "pressed");
+                yandexTranslateRequestBuilder.clearQuery();
+                yandexTranslateRequestBuilder.appendQueryParameter("key", getString(R.string.api_key))
+                        .appendQueryParameter("text", "hello")
+                        .appendQueryParameter("lang", "en-ru");
+                String yandexTranslateRequestURL = yandexTranslateRequestBuilder.build().toString();
+                JsonObjectRequest jsObjReq = new JsonObjectRequest
+                        (Request.Method.GET, yandexTranslateRequestURL, null, new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d("resultJson", response.toString());
+
+
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("resultJson", "error");
+                            }
+                        });
+                MySingleton.getInstance(getActivity()).addToRequestQueue(jsObjReq);
+                return false;
+            }
+        });
+        constraintLayout.setVisibility(View.INVISIBLE);
         spinnerFrom.setAdapter(adapter);
         spinnerTo.setAdapter(adapter);
+        yandexTranslateRequestBuilder = new Uri.Builder();
+        yandexTranslateRequestBuilder.scheme("https")
+                .authority("translate.yandex.net")
+                .appendPath("api")
+                .appendPath("v1.5")
+                .appendPath("tr.json")
+                .appendPath("translate");
         buttonClear.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Log.d("buttonClear","click");
+                textField.getText().clear();
+                constraintLayout.setVisibility(View.VISIBLE);
             }
         });
     }
